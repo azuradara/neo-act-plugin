80c541e282bab371a60934f3c04d7907f61ba7d8
Aymane Dara Hlamnach <dara-@outlook.jp>
2025-04-11 23:15:01 +0100

    chore: bump version

--- NeoActPlugin/Properties/AssemblyInfo.cs
+++ NeoActPlugin/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.6.0")]
+[assembly: AssemblyVersion("0.2.7.0")]
 
 [assembly: InternalsVisibleTo("NeoActPlugin.Test")]
